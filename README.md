f792ad5e2aebe56efa328644204017ed2ef2f7c4
Yasser <yasser_dahy @hotmail.fr>
2022-05-16 22:06:19 +0200

    Security check added in fields

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #################### JavaScript Project : grocery list ####################
 
-A static web site to make a grocery list allowing us to add, remove, edit items and clear the wole list by manipulating HTML DOM and NodeLists.
+A static web site to make a grocery list allowing us to add, remove, edit items and clear the whole list by manipulating HTML DOM and NodeLists. Some security features are implemented to avoid malicious script injection, that is why no special character is allowed in any field.
--- quickList_js/js/script.js
+++ quickList_js/js/script.js
@@ -1,11 +1,43 @@
 var list = document.getElementById("list");
 var listBody = document.getElementById("list-body");
+var security = 1;
+
+
+/******** Security ********/
+
+
+function htmlEncode(str) {
+  return String(str).replace(/[^\w. ]/gi, function(c){
+    return '&#'+c.charCodeAt(0)+';';
+  });
+}
+
+function jsEscape(str) {
+  return String(str).replace(/[^\w. ]/gi, function(c){
+    return '\\u'+('0000'+c.charCodeAt(0).toString(16)).slice(-4);
+  });
+}
+
+function securityCheck(str) {
+	if (htmlEncode(str) !== str && jsEscape(str) !== str) {
+		alert("Please only enter common letters and digits in the field.");
+
+		return security = 0;
+	}
+}
 
 
 /******** Add ********/
 
 
 function addItem(x) {
+
+	securityCheck(x);
+	if (security == 0) {
+		document.getElementById('new-item').value = ""; // clear the bar
+		security = 1;
+		return; // used to exit function
+	}
 
 	if(x.trim() != "") { // empty strings (including space) are not allowed
 		var itemStack = document.createElement("div");
@@ -96,17 +128,27 @@ function editItem() {
 		item.insertBefore(edition, toEdit);
 		item.insertBefore(submition, toEdit);
 
+
 		submition.addEventListener("click", function() {
 
-			if (edition.value.trim() != "") {
-				toEdit.innerHTML = edition.value; 
+			securityCheck(edition.value);
+			if (security == 0) {
 				edition.remove();
 				submition.remove();
 				toEdit.style.display = "";
+
+				security = 1;
 			} else {
-				edition.remove();
-				submition.remove();
-				toEdit.style.display = "";
+				if (edition.value.trim() != "") {
+					toEdit.innerHTML = edition.value; 
+					edition.remove();
+					submition.remove();
+					toEdit.style.display = "";
+				} else {
+					edition.remove();
+					submition.remove();
+					toEdit.style.display = "";
+				}
 			}
 		
 		});
